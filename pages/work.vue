a3047fd55e9213cc4adcf1ce7c8840ca865a29da
Aaron Mead <aaronm14@gmail.com>
2020-08-03 21:15:01 -0400

    Consistent spacing with other pages, though it makes it more smooshed so not sure I like it

--- pages/work.vue
+++ pages/work.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <Header />
-    <div class="container my-work-container">
-      <h1 class="title">
+    <div class="container max-w-screen-lg my-work-container">
+      <h1 class="title text-5xl mb-2">
         My Work
       </h1>
       <work-item
@@ -181,26 +181,15 @@ p {
 }
 
 .title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
   display: block;
-  font-weight: 300;
-  font-size: 100px;
   color: #35495e;
   letter-spacing: 1px;
 }
 
 .subtitle {
   font-weight: 300;
-  font-size: 42px;
+  font-size: 36px;
   color: #526488;
   word-spacing: 5px;
   padding-bottom: 15px;
